df15c26541597c5ba3ed9277f15d21f2d4fba214
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-12-03 15:37:24 +0100

    Added basic script for loading an viewing video, wrong format atm

---
+++ load_and_view_video_file.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+def load_video_eager():
+    # Eager loaded data file (all in memory before continuing)
+    return np.load("Data/Ballenwerper_sync_380fps_006.npy")
+
+def load_video_lazy():
+    # Lazy loading means the data is only loaded into memory when it is needed (frame-by-frame)
+    return np.lib.format.open_memmap('Data/Ballenwerper_sync_380fps_006.npy', mode='r+')
+
+video_data = load_video_lazy()
+
+
+# Assuming the shape is (num_frames, height, width, channels)
+for frame in video_data:
+    # Display the frame
+    cv2.imshow('Video', frame)
+
+    # Wait for 25ms before moving to the next frame (40 FPS)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+# Release the display window
+cv2.destroyAllWindows()
