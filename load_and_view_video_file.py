b643ee18988a53900569ab55a79ac83dcaa3af9e
Mateo Van Geyt <mateo.vangeyt@gmail.com>
2024-12-10 13:58:33 +0100

    resized window

--- load_and_view_video_file.py
+++ load_and_view_video_file.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+from tenacity import retry_unless_exception_type
+
 
 def load_video_eager():
     # Eager loaded data file (all in memory before continuing)
@@ -9,13 +11,25 @@ def load_video_lazy():
     # Lazy loading means the data is only loaded into memory when it is needed (frame-by-frame)
     return np.lib.format.open_memmap('Data/Ballenwerper_sync_380fps_006.npy', mode='r+')
 
+def get_screen_resolution():
+    screen = cv2.namedWindow("Temp", cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty("Temp",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    screen_width = cv2.getWindowImageRect("Temp")[2]
+    screen_height = cv2.getWindowImageRect("Temp")[3]
+    cv2.destroyWindow("Temp")
+    return screen_width,screen_height
+
+
 video_data = load_video_lazy()
 
+screen_width, screen_height = get_screen_resolution()
 
 # Assuming the shape is (num_frames, height, width, channels)
 for frame in video_data:
     # Display the frame
-    cv2.imshow('Video', frame)
+    resizedFrame = cv2.resize(frame,(screen_width,screen_height),interpolation=cv2.INTER_AREA)
+    cv2.imshow('Video', resizedFrame)
+    oldframe = resizedFrame
 
     # Wait for 25ms before moving to the next frame (40 FPS)
     if cv2.waitKey(25) & 0xFF == ord('q'):
